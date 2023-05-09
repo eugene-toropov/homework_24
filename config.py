e29dcd6efdb383a4d4422b6b91ca304aac1cc85e
Eugene <toropov_evgenii@mail.ru>
2023-05-09 22:17:40 +0300

    hope it works v.12

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    DB_NAME = os.getenv('DB_NAME')
-    DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT', 5432)
-    SQLALCHEMY_DATABASE_URI = "postgresql://db_user:db_password@localhost:5438/db_name"
+    # DB_USER = os.getenv('DB_USER')
+    # DB_PASSWORD = os.getenv('DB_PASSWORD')
+    # DB_NAME = os.getenv('DB_NAME')
+    # DB_HOST = os.getenv('DB_HOST')
+    # DB_PORT = os.getenv('DB_PORT', 5432)
+    SQLALCHEMY_DATABASE_URI = "postgresql://db_user:db_password@postgres_db:5432/db_name"
     # SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
